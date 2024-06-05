a54c6d91694ed431c0d8fcdcdd1c59dd2240fffc
EliNygard <eli.nygard@gmail.com>
2024-06-05 11:32:32 +0200

    styling projects, adding js

--- index.mjs
+++ index.mjs
@@ -1,8 +1,21 @@
 fetch("projects.json")
 .then(response => response.json())
 .then(data => {
-    console.log(data.projects);
     console.log(data.contact);
-    // display projects
+
+    const projects = data.projects;
+    console.log(projects);
+    
+    projects.forEach((project) => {
+        const projectsList = document.getElementById("projects-list")
+        projectsList.append(displayProjects(project))
+    })
 })
+
+function displayProjects() {
+    const projectItem = document.createElement("li")
+
+
+    return projectItem
+}
 
