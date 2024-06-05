eff393ba52b6319bc5bd0478b235da1f316ac449
EliNygard <eli.nygard@gmail.com>
2024-06-05 14:09:33 +0200

    generate html for projects

--- index.mjs
+++ index.mjs
@@ -1,21 +1,76 @@
 fetch("projects.json")
-.then(response => response.json())
-.then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data.contact);
 
     const projects = data.projects;
     console.log(projects);
-    
+    const projectsList = document.getElementById("projects-list");
+    projectsList.innerHTML = "";
     projects.forEach((project) => {
-        const projectsList = document.getElementById("projects-list")
-        projectsList.append(displayProjects(project))
-    })
-})
+      const projectItem = generateProjectsHtml(project);
+      projectsList.appendChild(projectItem);
+    });
+  });
 
-function displayProjects() {
-    const projectItem = document.createElement("li")
+function generateProjectsHtml(project) {
+  const projectItem = document.createElement("li");
+  projectItem.classList.add("project-item");
 
+  const linkWrapper = document.createElement("div");
+  linkWrapper.classList.add("link-wrapper");
 
-    return projectItem
+  const img = document.createElement("img");
+  img.src = project.screenshot.img;
+  img.alt = project.screenshot.alt;
+
+  const projectTextContent = document.createElement("div");
+  projectTextContent.classList.add("project-text-content");
+
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("title");
+
+  const projectTitle = document.createElement("h4");
+  projectTitle.textContent = project.title;
+
+  const linkIcon = document.createElement("i");
+  linkIcon.classList.add("fa-solid", "fa-square-arrow-up-right");
+
+  const description = document.createElement("p");
+  description.textContent = project.description;
+
+  const languagesList = document.createElement("ul");
+  languagesList.classList.add("languages", "newsreader-font");
+
+  const languages = project.technologies;
+  languages.forEach((language) => {
+    const item = document.createElement("li");
+    item.textContent = language;
+    console.log(item);
+    languagesList.appendChild(item);
+  });
+
+  const gitLink = document.createElement("a");
+  gitLink.title = "GitHub Repository";
+  gitLink.classList.add("github-link");
+
+  const gitLinkText = document.createElement("p");
+  gitLinkText.classList.add("uppercase");
+  gitLinkText.textContent = "View code";
+
+  const gitIcon = document.createElement("i");
+  gitIcon.classList.add("fa-brands", "fa-github");
+
+  projectItem.appendChild(linkWrapper);
+  linkWrapper.append(img, projectTextContent);
+  projectTextContent.append(
+    titleContainer,
+    description,
+    languagesList,
+    gitLink
+  );
+  titleContainer.append(projectTitle, linkIcon);
+  gitLink.append(gitLinkText, gitIcon);
+
+  return projectItem;
 }
-
